cc1650db7da2964a7c67744ed7e84a3f92ad63da
akoshel <johndoe@example.com>
2021-06-05 15:49:52 +0300

    albumetation

--- segmentation/models.py
+++ segmentation/models.py
@@ -2,8 +2,9 @@ import segmentation_models_pytorch as smp
 
 
 def get_model():
+    # TODO TIP: There's a lot of tasty things to try here.
     model = smp.Unet(
-        encoder_name="efficientnet-b5",
+        encoder_name="resnet34",
         encoder_weights="imagenet",
         in_channels=3,
         classes=1
